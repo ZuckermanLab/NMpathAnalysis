310f48724e8e93a7f01154a9067d6534f2ca94fd
Ernesto <ernesto.suarez.a@gmail.com>
2016-08-03 09:16:38 -0400

    Added DiscretePathEnsemble, changed the __init__ in Ensemble, the ensembles are now lists of arrays, created check_shape

--- nmtools/functions.py
+++ nmtools/functions.py
@@ -3,67 +3,119 @@ Created on Jul 29, 2016
 
 '''
 import numpy as np
-from random import random as r
+from random import random
 
-def get_sequence_shape(sequence, n_variables):
+def weighted_choice(list, weights = None):
     '''
-    "Automatically" figures out if the ensemble given is 
-    a set of trajectories or a single ensemble
+    Selects an element from a list given with probability given by
+    the variable weights
     '''
-    shape_error_message = '\nThe shape of the array given is not as expected, the shape should be:\n' + \
-                      'For list of trajectories -> (n_trajectories, n_snapshots, n_variables) if n_variables > 1, or \n' +\
-                      'For list of trajectories -> (n_trajectories, n_snapshots) if n_variables = 1, or\n' + \
-                      'For a ensemble -> (n_snapshots, n_variables) if n_trajectories = 1 and n_variables > 1, or\n' + \
-                      'For a ensemble -> (n_snapshots,) if n_trajectories = 1 and n_variables = 1 \n'
     
-    shape = sequence.shape
+    size = len(list)
+    if weights is not None:
+        assert(size == len(weights))
     
-    is_list_of_trajs = False
+    if weights is None:
+        probs = np.array([1/float(size) for i in range(size)])
+    else:
+        probs = np.array(weights)/sum(weights) # just in case they are not normalized
     
-    if len(shape) > 3:
-        raise Exception(shape_error_message)
+    rand = random()
     
-    if n_variables == 1:
-        if len(shape) == 2:
-            n_trajs = shape[0]
-            n_snapshots = shape[1]
-            is_list_of_trajs = True
-            return n_trajs, n_snapshots, is_list_of_trajs
-        elif len(shape) == 1:
-            n_trajs = 1
-            n_snapshots = shape[0]
-            return n_trajs, n_snapshots, is_list_of_trajs
+    _sum = 0
+    for i in range(size):
+        if _sum <= rand < _sum + probs[i]:
+            choice = i
+            break
         else:
-            raise Exception(shape_error_message)
-    elif n_variables > 1:
-        if len(shape) == 3:
-            n_trajs = shape[0]
-            n_snapshots = shape[1]
-            assert(n_variables == shape[2])
-            is_list_of_trajs = True
-            return n_trajs, n_snapshots, is_list_of_trajs
-        elif len(shape) == 2:
-            n_trajs = 1
-            n_snapshots = shape[0]
-            assert(n_variables == shape[1])
-            return n_trajs, n_snapshots, is_list_of_trajs
+            _sum += probs[i]
     
+    return list[choice]
+        
+def check_shape(trajectory):
+     
+    shape = trajectory.shape
+    
+    if len(shape) == 1:
+        n_snapshots = shape[0] 
+        n_variables =1
+        if n_variables == 0:
+            raise Exception('The shape {} of the trajectory/array given is not as expected'.format(shape))
+    elif len(shape) == 2:
+        n_snapshots = shape[0]
+        n_variables = shape[1]
+    else: 
+        raise Exception('The shape {} of the trajectory/array given is not as expected'.format(shape))
+
+    return n_snapshots, n_variables
+    
+
+# def markov_mfpt_from_fluxes(transition_matrix, ini_state, final_state):
+#     '''
+#     Computes the mfpt for systems with no absorbing states from 
+#     the transtion matrix
+# 
+#     '''
+#     transition_matrix = np.array(transition_matrix)
+#     
+#     n_states = len(transition_matrix)
+#     
+#     #pseudo non-markovian matrix (auxiliar_matrix)
+#     auxiliar_matrix = np.zeros((2*n_states,2*n_states))
+#     
+#     for i in range(2*n_states):
+#         for j in range(2*n_states):
+#             auxiliar_matrix[i,j] = transition_matrix[int(i/2),int[j/2]]
+#     
+#     for i in range(n_states):
+#         for j in range(n_states):
+#             if (i in final_state) or (j in final_state):
+#                 auxiliar_matrix[2*i,2*j] = 0.0
+#             if (i in ini_state) or (j in ini_state):
+#                 auxiliar_matrix[2*i+1,2*j+1] = 0.0
+#             if (not (j in ini_state)) or (i in ini_state):
+#                 auxiliar_matrix[2*i+1,2*j] = 0.0
+#             if (not (j in final_state)) or (i in final_state):
+#                 auxiliar_matrix[2*i,2*j+1] = 0.0
+#                  
+#     # the following line is going to return a MARKOVIAN mfpt since the auxiliar
+#     # matrix was build from a markovian matrix 
+#     return nonmarkov_mfpt_from_fluxes(auxiliar_matrix, ini_state, final_state)
+# 
+# 
+# def nonmarkov_mfpt_from_fluxes(nm_transition_matrix, ini_state, final_state):
+#     
+#     labeled_pops = pops_from_markov(nm_transition_matrix)
+#     
+#     flux = 0
+#     
+#     for i in range(0, 2*n_states, 2):
+#         for j in range(2*n_states):
+#             if int(j/2) in final_state:
+#                 flux += labeled_pops[i] * nm_transition_matrix[i,j]
+#     
+# 
+# def pops_from_markov(rate_matrix):
+#     "This a fake function"
+#     return [1/float(len(rate_matrix)) for i in range(len(rate_matrix))]
 
 ## To test the code    
 if __name__ ==  '__main__':
-    n_var = 2
-    n_snapshots  = 100
-    n_trajs = 1
-    #
-    t = np.array([ [r() for i in range(n_var)] for j in range(n_snapshots)])
-    s = [t for i in range(n_trajs)]
-    s = np.array(s)
-    s = np.array([])
-    print('input shape:', s.shape)
-    
-    shape2 = get_sequence_shape(s, n_var)
-    
-    print("\n n_trajs = {}, n_snapshots = {} and n_var = {}".format(shape2[0],shape2[1],n_var))
+    pass
+#     n_var = 2
+#     n_snapshots  = 100
+#     n_trajs = 1
+#     #
+#     t = np.array([ [random() for i in range(n_var)] for j in range(n_snapshots)])
+#     s = [t for i in range(n_trajs)]
+#     s = np.array(s)
+#     s = np.array([])
+#     print('input shape:', s.shape)
+#     
+#     shape2 = get_sequence_shape(s, n_var)
+#     
+#     print("\n n_trajs = {}, n_snapshots = {} and n_var = {}".format(shape2[0],shape2[1],n_var))
+
     
     
     
--- src/ensemble.py
+++ src/ensemble.py
@@ -2,50 +2,65 @@
 
 import numpy as np
 import random
+from copy import deepcopy
+import networkx as nx
 
 from NMpathAnalysis.nmtools.interval import Interval
-from NMpathAnalysis.nmtools.functions import get_sequence_shape
+from NMpathAnalysis.nmtools.functions import check_shape, weighted_choice
 
 class Ensemble:
     '''
-    Stores a array of space-continuous trajectories (Ensemble).
-    If a single trajectory is given, is going to transform it to a list/array of trajs
-    with only one element/trajectory.
+    Stores a list of space-continuous trajectories.
     '''
     
-    def __init__(self, sequence, n_variables = 1):
-        
-        self.sequence = np.array(sequence) # a list of arrays is saved
-        
-        if self.sequence.shape[0] == 0:
-            # the sequence is an empty list
+    def __init__(self, trajectory = None, verbose = False, dtype ='float32'):
+        '''
+        trajectory:      is a single trajectory that can be used to instantiate      
+        '''
+        if trajectory is None:
+            self.trajectories = []
             self.n_variables = 0
+            if verbose: print('\nEmpty ensemble generated')
         else:
-            n_trajectories, n_snapshots, is_list_of_trajs = get_sequence_shape(self.sequence, n_variables)
+            trajectory = np.array(trajectory, dtype = dtype)
+            n_snapshots, n_variables = check_shape(trajectory)
             self.n_variables = n_variables
+            self.trajectories = [ trajectory ]
             
-            if not is_list_of_trajs:
-                self.sequence = np.array([self.sequence])  # make it a list/array of trajs
-      
+            if verbose:
+                print('\nSingle trajectory was read with shape {}'.format(trajectory.shape) )
+                print('n_snapshots = {}, n_variables = {}'.format(n_snapshots, self.n_variables))
+    
+    def add_trajectory(self, trajectory):
+        '''
+        Add a trajectory to the ensemble
+        '''
+        if not isinstance(trajectory, np.ndarray):
+            trajectory = np.array(trajectory)
+
+        _n_snapshots, _n_variables = check_shape(trajectory)
+
+        if self.n_variables == 0: # Empty ensemble
+            self.trajectories = [trajectory] 
+        else:
+            if self.n_variables != _n_variables:
+                raise Exception('All the trajectories in the same ensemble must have the same number of variables')
+            else:
+                self.trajectories.append(trajectory)
+                      
     def __len__(self):
         #returns the number of trajectories in the ensemble
-        return len(self.sequence)
+        return len(self.trajectories)
     
-    def __add__(self,other):
-        if (self.n_variables != other.n_variables) and self.n_variables != 0 and other.n_variables != 0:
-            raise Exception('To add two not-empty trajectories, the number of variables should be the same')
+    def __add__(self, other):
+        ensemble_sum = deepcopy(self)
         
-        if (self.sequence.shape[0] == 0) and (other.sequence.shape[0] == 0): # both arrays are empty
-            return Ensemble(self.sequence)
-        elif (self.sequence.shape[0] == 0) and (other.sequence.shape[0] != 0): # only other is empty
-            return Ensemble(other.sequence)
-        elif (self.sequence.shape[0] != 0) and (other.sequence.shape[0] == 0):
-            return Ensemble(self.sequence)
-        else:
-            sum_seq = np.append(self.sequence, other.sequence, axis = 0)
-            return Ensemble(sum_seq)
-        
-    def __iadd__(self,other):
+        for traj in other.trajectories:
+            ensemble_sum.add_trajectory(traj)
+            
+        return ensemble_sum
+         
+    def __iadd__(self, other):
         return self.__add__(other)
     
     def mfpts(self, stateA = None, stateB = None): 
@@ -65,7 +80,7 @@ class Ensemble:
         passageTimeBA=[]
         fpt_counter = 0  # first passage time counter
         
-        for traj in self.sequence:
+        for traj in self.trajectories:
             previous_color = "Unknown"
             state = "Unknown"
             for snapshot in traj:
@@ -114,13 +129,13 @@ class Ensemble:
 
     #def populations(self,binbounds):
         
-    def _count_matrix(self, n_states, map_function = None):
+    def _count_matrix(self, n_states = None, map_function = None):
         if (map_function is None) or (n_states is None):
-            raise ValueError('The number of states and a map function have to be given as argument')
+            raise Exception('The number of states and a map function have to be given as argument')
 
         count_matrix = np.zeros((n_states, n_states))
         
-        for traj in self.sequence:
+        for traj in self.trajectories:
             previous_state = "Unknown" 
             for snapshot in traj:
                 current_state = map_function(snapshot)
@@ -142,48 +157,49 @@ class Ensemble:
                 transition_matrix[i,:] = transition_matrix[i,:]/row_sum
                 
         return transition_matrix
-        
+
+    
 class DiscreteEnsemble(Ensemble):
     '''
     Discrete trajectory
     '''
-    def __init__(self, sequence):
-        n_variables = 1 #the state of a discrete trajectories must have only one index
-        self.sequence = np.array(sequence,dtype = np.int32) # a list of arrays is saved
-        n_trajectories, n_snapshots, is_list_of_trajs = get_sequence_shape(self.sequence, n_variables)
-        
-        if not is_list_of_trajs:
-            self.sequence = np.array([self.sequence]) # make it a list
-        self.n_variables = n_variables
+    def __init__(self, trajectory = None, verbose = False, dtype = 'int32'):
+        super().__init__(trajectory, verbose, dtype)
+        if (self.n_variables !=1) and (self.n_variables != 0): # In this case is just an index
+            raise Exception('A discrete trajectory must have a one-dimensional index/variable unless is empty')
     
     @classmethod
-    def from_ensemble(cls, ens, map_function = None):
+    def from_ensemble(cls, ens, map_function = None , dtype = 'int32'):
         '''
         Build a DiscreteEnsemble from an ensemble object or a single trajectory
         '''
         if map_function is None:
             raise Exception('A map function has to be given as argument')
         
-        discrete_sequence = [] # list of ensembles
+        discrete_ensemble = cls()
         
         if isinstance(ens, Ensemble):
             #it is an Ensemble object
-            for seq in ens.sequence:
-                temp_traj = np.array([], dtype=np.int32)
-                for snapshot in seq:
-                    temp_traj = np.append(temp_traj, [map_function(snapshot)], axis = 0)
-                discrete_sequence += [temp_traj]
+            for traj in ens.trajectories:
+                d_traj = np.array([], dtype = dtype)
+                for snapshot in traj:
+                    d_traj = np.append(d_traj, [map_function(snapshot)], axis = 0)
+                discrete_ensemble.add_trajectory(d_traj)
         else:
             # it is a single trajectory or array
+            d_traj = []
             for snapshot in ens:
-                discrete_sequence += [map_function(snapshot)]
+                d_traj += [map_function(snapshot)]
+            d_traj = np.array(d_traj, dtype = dtype)
+            discrete_ensemble.add_trajectory(d_traj)
+            
 
-        return cls(discrete_sequence)
+        return discrete_ensemble
     
     @classmethod
-    def from_transition_matrix(cls, transition_matrix, sim_length = None, initial_state = 0):
+    def from_transition_matrix(cls, transition_matrix, sim_length = None, initial_state = 0, dtype = 'int32'):
         '''
-        Generates a trajectory from the transition matrix
+        Generates a discrete ensemble from the transition matrix
         '''
         if sim_length is None:
             raise Exception('The simulation length must be given')
@@ -192,24 +208,16 @@ class DiscreteEnsemble(Ensemble):
             transition_matrix = np.array(transition_matrix)
         
         n_states = len(transition_matrix)
-        assert(len(transition_matrix) == len(transition_matrix[0]))
+        assert(n_states == len(transition_matrix[0]))
         
         current_state = initial_state
         discrete_traj = np.array([initial_state],dtype=np.int32)
         
         for i in range(sim_length):
-            rand_num = random.random()
-            partial_sum = 0.0
+            next_state = weighted_choice([k for k in range(n_states)],transition_matrix[current_state,:])
+            discrete_traj = np.append(discrete_traj, [next_state], axis = 0)
+            current_state = next_state
             
-            for j in range(n_states):
-                if partial_sum <= rand_num < ( partial_sum + transition_matrix[current_state,j] ):
-                    current_state = j
-                    break
-                else:
-                    partial_sum += transition_matrix[current_state,j]
-                    
-            discrete_traj = np.append(discrete_traj, [current_state], axis = 0)
-        
         return cls(discrete_traj)
 
 
@@ -217,7 +225,7 @@ class DiscreteEnsemble(Ensemble):
 
         count_matrix = np.zeros((n_states, n_states))
         
-        for traj in self.sequence:
+        for traj in self.trajectories:
             for i in range(len(traj)-1):
                 count_matrix[traj[i], traj[i+1]] += 1.0
 
@@ -238,10 +246,79 @@ class DiscreteEnsemble(Ensemble):
                 
         return transition_matrix
 
-class PathEnsemble(Ensemble):
-    pass
-    
+class DiscretePathEnsemble(DiscreteEnsemble):
 
+    @classmethod
+    def from_transition_matrix(cls, transition_matrix, ini_state, final_state, n_paths = 1000, ini_pops = None,  max_iters = 1000000000, dtype = 'int32'):
+        '''
+        Construct a path ensemble from a transition matrix
+        
+        ini_state:        list, intitial state
+        
+        final_state:      list, final state
+        
+        ini_pops:         list or label, probability distribution over the 
+                          initial state used to generate the path
+                          
+                          possible values:
+                          ----------------
+                          a)  None
+                              Use a uniform distribution over the states in ini_state
+                              
+                          b) 'ss'
+                              Use the steady state solution (first eigenvector) of
+                              the transtion matrix
+                              
+                          c) list
+                              A list with the explicit values of the populations in ini_state 
+                              that should be used to generate the ensemble
+        '''
+        
+        if ini_pops is None:
+            ini_pops = [1/float(len(ini_pops)) for i in range(len(ini_pops))]
+        elif ini_pops == 'ss':
+            raise NotImplementedError('Sorry: not yet implemented')
+        
+        
+        cls.transtion_matrix = transition_matrix
+        cls.ini_state = ini_state
+        cls.final_state = final_state
+        cls.ini_pops = ini_pops
+        cls.max_iters = max_iters
+        cls.n_paths = n_paths
+
+        n_states = len(transition_matrix)
+        assert(n_states == len(transition_matrix[0]))
+        
+        d_trajectories = []
+        
+        for i in range(n_paths):
+            current_state = weighted_choice(ini_state, ini_pops) #Initial state
+            path = [current_state]
+            
+            for j in range(max_iters):
+                next_state = weighted_choice([k for k in range(n_states)],transition_matrix[current_state,:])
+                path += [ next_state ]
+                current_state = next_state
+                if j+1 == max_iters:
+                    print('\nWARNING: max iteration reached when generating the path ensemble, consider to increase max_iters')
+                if (current_state in final_state): break
+                
+            path = np.array(path,dtype = dtype)
+            d_trajectories += [path]
+        
+        cls.trajectories = d_trajectories
+        
+        return cls
+
+#     def fundamental_sequences(self, _transition_matrix = None):
+#         
+#         if _transition_matrix is None:
+#             transtion_matrix = self.transition_matrix
+#         
+#         for path in self.trajectories:
+            
+        
 '''
 ------------------------------------------------------
 EVERYTHING FROM HERE IS JUST TO TEST THE CODE ABOVE
@@ -254,7 +331,7 @@ def mc_simulation(numsteps):
     mc_traj = []
     
     for i in range(numsteps):
-        dx = random.uniform(-10,10)
+        dx = np.random.uniform(-10,10)
         if (x + dx) in I:
             x = x + dx
         mc_traj.append(x)
@@ -268,60 +345,31 @@ def simple_mapping2(x):
     
 
 def main():
-    #test_trajectory = [random.random()*100 for i in range(100000)]
+
     test_trajectory0 = mc_simulation(10000)
     test_trajectory1 = mc_simulation(10000)
     test_trajectory2 = mc_simulation(10000)
-    #print(test_trajectory)
-    #################################################
+
     stateA = [0,10]
     stateB = [90,100]
     ensemble0 = Ensemble(test_trajectory0)
     print('\nmfpts from the continuous simulation: given t0')
     print(ensemble0.mfpts(stateA, stateB))
-
-    ##################################################
-    list_of_trajs = [test_trajectory1, test_trajectory2]
-    ensemble_tot = Ensemble(list_of_trajs)
-    #transition_matrix = ensemble_tot._mle_transition_matrix(10,simple_mapping2)
-    print('\nmfpts from the continuous simulation: given [t1,t2]')
-    print(ensemble_tot.mfpts(stateA, stateB))
+    print('\nNum of trajs: ',len(ensemble0))
     
-    #######################################
-    e1 = Ensemble(test_trajectory1)
-    e2 = Ensemble(test_trajectory2)
+    ensemble0.add_trajectory(test_trajectory1)
+    print('\nNum of trajs: ',len(ensemble0))
     
-    ensemble_tot = e1 + e2
+    ensemble2 = Ensemble(test_trajectory2,verbose=True)  
     
-    print('\nmfpts from the continuous simulation: given Ensemble(t1) + Ensemble(t2)')
-    print(ensemble_tot.mfpts(stateA, stateB))
-    
-    ##########################################
-    d_ensemble_0 = DiscreteEnsemble.from_ensemble(test_trajectory0, simple_mapping2)
-    
-    stateA = [0]
-    stateB = [9]
-    
-    print('\nmfpts from the discrete simulation, optained from t0')
-    print(d_ensemble_0.mfpts(stateA, stateB))
+    ensemble_tot = ensemble0 + ensemble2
+    print('\nNum of trajs: ',len(ensemble_tot))
+    print(np.array(ensemble_tot.trajectories).shape)
     
     
-    #############################################
-
-    
-    
-    #d_from_T = DiscreteEnsemble.from_transition_matrix(T_matrix, 10000)
     
 
-    
-
-    
-    #print('\nmfpts from the simulation -> discrete_sim -> transition_matrix -> discrete simulation')
-    #print(d_from_T.mfpts(stateA,stateB))
-
-    
 if __name__ == '__main__':
-    from interval import Interval
     main()
         
     
