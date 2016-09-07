27c0802457793cd535abdc7cf39375389e375e4a
Ernesto Suarez <ernesto.suarez.a@gmail.com>
2016-09-06 23:42:27 -0400

    changes in __init__

--- nmpath/msm.py
+++ nmpath/msm.py
@@ -6,9 +6,9 @@ Created on Jul 28, 2016
 import numpy as np
 
 class MarkovStateModel:
-    '''
-    Basic Markov model. Computes basic properties from a Markov model
-    constructed from a list of 1D sequences of integers
+    '''Markov State Model
+    ------------------
+    Fits a regular Markov model from a list of 1D sequences of integers
     
     For example:
     
@@ -17,15 +17,44 @@ class MarkovStateModel:
     If only one sequence is given in sequences, the format has to be the same:
     
     sequences = [ [1 , 2, 0, ...] ]
-    '''
 
-    def __init__(self, lag_time=1):
+    Parameters
+    ...........
+    lag_time (integer, default: 1) 
+        Lag time of the model.
+    reversible (boolean, default: true) 
+        Enforce the reversibility of the transition matrix.
+        Method by which the reversibility of the transition matrix
+        is enforced. 'mle' uses a maximum likelihood method that is
+        solved by numerical optimization, and 'transpose'
+        uses a more restrictive (but less computationally complex)
+        direct symmetrization of the expected number of counts.
+    prior_counts (integer)
+        Add a number of "pseudo counts" to each entry in the counts matrix
+        after ergodic trimming.  When prior_counts == 0 (default), the assigned
+        transition probability between two states with no observed transitions
+        will be zero, whereas when prior_counts > 0, even this unobserved
+        transitions will be given nonzero probability.
+    sliding_window (boolean)
+        Count transitions using a window of length ``lag_time``, which is slid
+        along the sequences 1 unit at a time, yielding transitions which
+        contain more data but cannot be assumed to be statistically
+        independent. Otherwise, the sequences are simply subsampled at an
+        interval of ``lag_time``.
+    '''
+    
+
+    def __init__(self, lag_time=1, reversible_type=None, prior_counts=0, sliding_window=True):
         '''
         sequence: 
             Sequence of integers. Is a 1D discrete ensembles or 
         '''
-        self.count_matrix = None
+        self.reversible_type = reversible_type
         self.lag_time = lag_time
+        self.sliding_window = sliding_window
+        self.prior_counts = prior_counts
+
+
         
     def build(self, n_sates, sequences):
         '''
