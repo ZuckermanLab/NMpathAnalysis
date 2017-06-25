a71eeb0d9f654c01dbc48591d359ec08a77de562
Ernesto Suarez <ernesto.suarez.a@gmail.com>
2017-06-24 21:29:49 -0400

    Format changes

--- nmpath/interval.py
+++ nmpath/interval.py
@@ -5,66 +5,71 @@ Created on Jul 28, 2016
 '''
 import numpy as np
 
+
 class Interval:
     '''Intervals are in general defined as half-open interval [start,end),
     in any case, in each dimension the interval is specified using a list [a,b]
     where a < b
-    
-    - For 1D interval a single list in the form [a,b] has to be given 
-    
-    - A list of lists [[a,b],[c,d],...] are used for a n-dimensional 
-        intervals, one for each dimension, i.e, len(inteval) = n_variables 
-        
-    - A list-of-lists-of-lists for the mathematical union of n-dimensional intervals' 
-    
+
+    - For 1D interval a single list in the form [a,b] has to be given
+
+    - A list of lists [[a,b],[c,d],...] are used for a n-dimensional
+        intervals, one for each dimension, i.e, len(inteval) = n_variables
+
+    - A list-of-lists-of-lists for the mathematical union of n-dimensional
+        intervals'
+
         [ [[a,b],[c,d],...],  [[e,f],[g,h],...], ... ]
-    
+
     '''
-    
+
     def __init__(self, range):
         self.range = range
-            
 
     def __contains__(self, item):
-        
+
         shape = np.array(self.range).shape
-        
-        if len(shape) == 1: # 1D interval
+
+        if len(shape) == 1:  # 1D interval
             if self.range[0] != self.range[1]:
                 try:
                     return self.range[0] <= item and item < self.range[1]
-                except: 
-                    raise Exception('The given interval has not the expected shape')
+                except:
+                    raise Exception('The given interval has not the \
+                    expected shape')
             else:
                 return item == self.range[0]
-            
-        elif len(shape) == 2: # n-dimensional interval
+
+        elif len(shape) == 2:  # n-dimensional interval
             if len(self.range) != len(item):
-               raise Exception('The given interval has not the expected shape')
+                raise Exception('The given interval has not \
+                the expected shape')
             else:
-                if all([  (item[i] in Interval(self.range[i])) for i in range(len(item))   ]):
-                    return True  
-                
-        elif len(shape) == 3: # union of n-dimensional intervals
-            if any( [(item in Interval(self.range[i])) for i in range(len(self.range))]   ):
+                if all([(item[i] in Interval(self.range[i])) for i in range(len(item))]):
+                    return True
+
+        elif len(shape) == 3:  # union of n-dimensional intervals
+            if any([(item in Interval(self.range[i])) for i in range(len(self.range))]):
                 return True
             else:
                 return False
         else:
             raise Exception('The given interval has not the expected shape')
-    
+
     def __iter__(self):
         return iter(self.range)
-    
+
     def __getitem__(self, arg):
         return self.range[arg]
 
+
 if __name__ == '__main__':
-    I = Interval([[2,3],[1,2]])
-    I = Interval([ [[2,3],[1,2]] ,[[1,2],[0,1]] ])
-    #I = Interval((1,2))
-    
-    if [1.5,2] in I:
+
+    I = Interval([[2, 3], [1, 2]])
+    I = Interval([[[2, 3], [1, 2]], [[1, 2], [0, 1]]])
+    # I = Interval((1,2))
+
+    if [1.5, 2] in I:
         print('yes')
     else:
         print('no')
