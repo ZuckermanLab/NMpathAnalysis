b8b6fd2db26d0c35724a68e44920c510813c3b2c
Ernesto Suarez <ernesto.suarez.a@gmail.com>
2017-06-25 11:07:36 -0400

    Non Markovian model first commit

--- nmpath/nmm.py
+++ nmpath/nmm.py
@@ -5,34 +5,34 @@ Created on Jul 28, 2016
 
 import numpy as np
 
-class MarkovStateModel:
-    '''Markov State Model
+
+class NonMarkovModel:
+
+    '''Non Markovian Model
     ----------------------
-    **API Based on MSMBuilder sofware (this is a simplified version)
-    https://github.com/msmbuilder/msmbuilder
 
     Fits a regular Markov model from a list of 1D sequences of integers
-    
+
     For example:
-    
+
     sequences = [ [1 , 2, 0, ...], [2, 2, 1, ...], [3, 1, 2, ...], ...]
-    
+
     If only one sequence is given in sequences, the format has to be the same:
-    
+
     sequences = [ [1 , 2, 0, ...] ]
 
     Parameters
     ----------
-    lag_time (integer, default: 1) 
+    lag_time (integer, default: 1)
         Lag time of the model.
     reversible_type: {'transpose','mle','revmle'}
         Enforce the reversibility of the transition matrix.
-        
+
         possible values:
         ---------------
         - 'mle': If selected, the reversibility is not enforced (regular MLE)
         - 'transpose': the count matrix is averaged with its transposed.
-        - 'revmle': Maximum likelihood estimator for reversible 
+        - 'revmle': Maximum likelihood estimator for reversible
                     matrix (J. Chem. Phys. 2011, 134, 174105)
 
     prior_counts (integer)
@@ -50,13 +50,12 @@ class MarkovStateModel:
         stores the number of transitions observed from i to j.
 
     populations: array, shape (n_states,)
-        Equilibrium population, the steady state solution of of the 
+        Equilibrium population, the steady state solution of of the
         transition matrix
     '''
-    
 
-    def __init__(self, lag_time=1, reversible_type=None, prior_counts=0, 
-                sliding_window=True):
+    def __init__(self, lag_time=1, reversible_type=None, prior_counts=0,
+                 sliding_window=True):
         self.reversible_type = reversible_type
         self.lag_time = lag_time
         self.sliding_window = sliding_window
@@ -66,45 +65,43 @@ class MarkovStateModel:
 
         if reversible_type is None:
             serevesble_type = 'mle'
-        else if reversible_type is not in ('mle','transpose','revmle'):
+        else if reversible_type is not in ('mle', 'transpose', 'revmle'):
             raise ValueError('Reversible type: {} is not valid', reversible_type)
-        
+
         self.reversible_type = reversible_type
 
         if (self.lag_time < 1) or (int(self.lag_time) != int(self.lag_time)):
             raise ValueError('The lag time should be an integer greater than 1')
 
-
-    def fit(sequence):
+    def estimate_nm_model(sequence, stateA, stateB):
         '''Fits the the markov model from a list of sequences
-        
-        '''
-        ## maps the given sequence to an internal sequence
-        #map = {}
 
+        '''
+        # maps the given sequence to an internal sequence
+        #map = {}
 
         pass
 
-
-        
     def build(self, n_sates, sequences):
         '''
         Compute count matrix
         '''
-        
+
         self.count_matrix = np.matrix((n_states, n_states))
-        
+
         for sequence in sequences:
             previous_state = "Unknown"
             for state in sequence:
                 if previous_state != "Unknown":
                     count_matrix[previous_state, state] += 1.0
             previous_state = current_state
-        
+
         return count_matrix
+
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
