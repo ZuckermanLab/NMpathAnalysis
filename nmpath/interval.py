5a0e385b8ef0cbf2d445a29b3fbc2a2db5f0ba64
Ernesto <ernesto.suarez.a@gmail.com>
2017-06-28 11:26:30 -0400

    fixed bug

--- nmpath/interval.py
+++ nmpath/interval.py
@@ -11,7 +11,10 @@ class Interval:
     in any case, in each dimension the interval is specified using a list [a,b]
     where a < b
 
-    - For 1D interval a single list in the form [a,b] has to be given
+    - For 1D (single) interval a single list in the form [a,b] has to be given
+
+    - The union of multiple (1D) intervals can be specified as:
+        [[a,b],[c,d],...]
 
     - A list of lists [[a,b],[c,d],...] are used for a n-dimensional
         intervals, one for each dimension, i.e, len(inteval) = n_variables
@@ -23,53 +26,64 @@ class Interval:
 
     '''
 
-    def __init__(self, range):
-        self.range = range
+    def __init__(self, interval_set, n_dim=None):
+        self.interval_set = interval_set
+        self.n_dim = n_dim
+        if n_dim is None:
+            raise Exception('The number of variables/dimensions has to be specified')
 
     def __contains__(self, item):
 
-        shape = np.array(self.range).shape
+        shape = np.array(self.interval_set).shape
 
-        if len(shape) == 1:  # 1D interval
-            if self.range[0] != self.range[1]:
-                try:
-                    return self.range[0] <= item and item < self.range[1]
-                except:
-                    raise Exception('The given interval has not the \
-                    expected shape')
-            else:
-                return item == self.range[0]
+        len_shape = len(shape)
 
-        elif len(shape) == 2:  # n-dimensional interval
-            if len(self.range) != len(item):
-                raise Exception('The given interval has not \
-                the expected shape')
-            else:
-                if all([(item[i] in Interval(self.range[i])) for i in range(len(item))]):
-                    return True
+        if (self.n_dim == 1) and (len_shape == 1):  # single 1D interval
+            return self.interval_set[0] <= item < self.interval_set[1]
+
+        elif (self.n_dim == 1) and (len_shape == 2):  # union of multiple 1D intervals
+            return any([(item in Interval(self.interval_set[i], 1)) for i in range(shape[0])])
+
+        elif (self.n_dim > 1) and len_shape == 2:  # n-dimensional interval
+            return all([(item[i] in Interval(self.interval_set[i], 1)) for i in range(shape[0])])
 
         elif len(shape) == 3:  # union of n-dimensional intervals
-            if any([(item in Interval(self.range[i])) for i in range(len(self.range))]):
-                return True
-            else:
-                return False
+            return any([(item in Interval(self.interval_set[i], self.n_dim)) for i in range(shape[0])])
         else:
             raise Exception('The given interval has not the expected shape')
 
     def __iter__(self):
-        return iter(self.range)
+        return iter(self.interval_set)
 
     def __getitem__(self, arg):
-        return self.range[arg]
+        return self.interval_set[arg]
 
 
 if __name__ == '__main__':
 
-    I = Interval([[2, 3], [1, 2]])
-    I = Interval([[[2, 3], [1, 2]], [[1, 2], [0, 1]]])
-    # I = Interval((1,2))
+    # 1D single interval
+    I = Interval([1, 2], 1)
+    assert(0.5 not in I)
+    assert(1.3 in I)
 
-    if [1.5, 2] in I:
-        print('yes')
-    else:
-        print('no')
+    I = Interval([[1, 2]], 1)  # should work too
+    assert(0.5 not in I)
+    assert(1.3 in I)
+
+    # 1D union of intervals
+    I = Interval([[1, 2], [3, 4]], 1)
+    assert(0.5 not in I)
+    assert(1.9 in I)
+    assert(3.3 in I)
+
+    # 3D single interval
+    I = Interval([[1, 2], [0, 1], [0.1, 0.2]], 3)
+    assert([1.5, 1.5, 0.15] not in I)
+    assert([1.5, 0.5, 0.15] in I)
+
+    # 3D union of intervals
+    I = Interval([[[1, 2], [0, 1], [0.1, 0.2]], [[2, 3], [1, 2], [0.1, 0.2]]], 3)
+    assert([1.5, 1.5, 0.15] not in I)
+    assert([1.5, 0.5, 0.15] in I)
+    assert([2.5, 1.5, 0.15] in I)
+    assert([2.5, 1.5, 0.3] not in I)
