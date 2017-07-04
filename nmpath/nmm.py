27c36238082c29faeb57d8691400a152d70e9247
Ernesto Suarez <ernesto.suarez.a@gmail.com>
2017-07-03 21:47:18 -0400

    fit() added

--- nmpath/nmm.py
+++ nmpath/nmm.py
@@ -4,8 +4,7 @@ Created on Jul 28, 2016
 '''
 
 import numpy as np
-from auxfunctions import map_to_integers
-from ensembles import Ensemble
+from auxfunctions import map_to_integers, normalize_markov_matrix
 
 
 class NonMarkovModel:
@@ -57,12 +56,11 @@ class NonMarkovModel:
     '''
 
     def __init__(self, trajectories=None, stateA=None, stateB=None,
-                 lag_time=1, clean_traj=True):
+                 lag_time=1):
         self.lag_time = lag_time
         self.trajectories = trajectories
         self.stateA = stateA
         self.stateB = stateB
-        self.clean_traj = clean_traj
 
         if (self.lag_time < 1) or (int(self.lag_time) != int(self.lag_time)):
             raise ValueError('The lag time should be an integer \
@@ -72,33 +70,77 @@ class NonMarkovModel:
 
     def map_trajectories_to_integers(self):
         # Clean the sequences
-        if self.clean_traj:
-            seq_map = {}
-            for seq in self.trajectories:
-                newseq, m_dict = map_to_integers(seq, seq_map)
-                seq = newseq
-            self._stateA = [seq_map[i] for i in self.stateA]
-            self._stateB = [seq_map[i] for i in self.stateB]
-        else:
-            self._stateA = self.stateA
-            self._stateB = self.stateB
+        seq_map = {}
+        new_trajs = []
+        for seq in self.trajectories:
+            newseq, m_dict = map_to_integers(seq, seq_map)
+            new_trajs.append(newseq)
+        self._stateA = [seq_map[i] for i in self.stateA]
+        self._stateB = [seq_map[i] for i in self.stateB]
+        self.n_states = len(seq_map)
+        self._trajectories = new_trajs
+        self.seq_map = seq_map
 
-    def mfpt(self, sequences, _stateA, _stateB):
-        '''Fits the the markov model from a list of sequences
+        self.fit()
 
+    def fit(self):
+        '''Fits the the non markovian model from a list of sequences
         '''
+        # Non-Markovian count matrix
+        nm_cmatrix = np.zeros((2 * self.n_states, 2 * self.n_states))
+
+        for traj in self._trajectories:
+
+            prev_color = None
+            for i, curr_microstate in enumerate(traj, start=self.lag_time):
+                prev_microstate = traj[i - self.lag_time]
+                # Macro state determination
+                if curr_microstate in self._stateA:
+                    state = "A"
+                elif curr_microstate in self._stateB:
+                    state = "B"
+                else:
+                    state = None
+
+                # Color determination
+                if state == "A":
+                    color = "A"
+                elif state == "B":
+                    color = "B"
+                else:
+                    color = prev_color
+
+                # Count matrix for the given lag time
+                if prev_color == "A" and color == "B":
+                    nm_cmatrix[2 * prev_microstate, 2 * curr_microstate + 1] += 1.0
+                elif prev_color == "B" and color == "A":
+                    nm_cmatrix[2 * prev_microstate + 1, 2 * curr_microstate] += 1.0
+                elif prev_color == "A" and color == "A":
+                    nm_cmatrix[2 * prev_microstate, 2 * curr_microstate] += 1.0
+                elif prev_color == "B" and color == "B":
+                    nm_cmatrix[2 * prev_microstate + 1, 2 * curr_microstate + 1] += 1.0
+
+                prev_color = color
+
+        nm_tmatrix = normalize_markov_matrix(nm_cmatrix)
+
+        self.nm_tmatrix = nm_tmatrix
+        self.nm_cmatrix = nm_cmatrix
 
 
-def main():
-    trajectories = [[3, 1, 5, 4]]
+if __name__ == '__main__':
+    from random import random as rd
+    from mfpt import non_markov_mfpts
+
+    trajectories = [np.random.randint(0, 9, 500)]
+    print(trajectories)
 
     model = NonMarkovModel(trajectories, stateA=[3], stateB=[4])
     print(model._stateA)
     print(model._stateB)
+    print(model.n_states)
 
-    for traj in model.trajectories:
+    for traj in model._trajectories:
         print(traj)
 
-
-if __name__ == '__main__':
-    main()
+    print(non_markov_mfpts(model.nm_tmatrix, model._stateA, model._stateB))
